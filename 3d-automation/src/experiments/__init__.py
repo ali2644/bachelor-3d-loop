ecb431d32f1836aeec89b05e24a14c3344b8d872
ali2644s <ali2644s@hs-coburg.de>
2026-08-15 10:26:13 +0200

    refactor: export ExperimentRunner from experiments package

--- 3d-automation/src/experiments/__init__.py
+++ 3d-automation/src/experiments/__init__.py
@@ -4,8 +4,10 @@ from experiments.experiment_plan import (
     ExperimentPlanEntry,
     load_experiment_plan,
 )
+from experiments.experiment_runner import ExperimentRunner
 
 __all__ = (
     "ExperimentPlanEntry",
+    "ExperimentRunner",
     "load_experiment_plan",
 )
